b948fd68494e477bb2d92b1f1577c32fa37b927e
Rafael Baron <rask2208@gmail.com>
2024-06-27 19:48:10 -0500

    Create logic to show/hide Test and Study boards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: '<app-home></app-home>',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'multip';
-}
+export class AppComponent {}
